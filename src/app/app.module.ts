57cf1362a81ed56769d30cb2a179cb999e20161a
MGraf <info@moritz-graf.de>
2019-11-06 17:00:22 +0100

    Add Component, Start Styling, ToDo: Finish Fontawesomeintegration

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,19 +2,23 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { AppComponent } from './app.component';
 import { TaskComponent } from './task/task.component';
 import { TaskListComponent } from './task-list/task-list.component';
+import { TaskListItemTeamComponent } from './task-list-item-team/task-list-item-team.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     TaskComponent,
-    TaskListComponent
+    TaskListComponent,
+    TaskListItemTeamComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FontAwesomeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/task-list-item-team/task-list-item-team.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit, Input } from '@angular/core';
+import { Task } from '../datatypes/Task';
+
+@Component({
+  selector: 'app-task-list-item-team',
+  templateUrl: './task-list-item-team.component.html',
+  styleUrls: ['./task-list-item-team.component.css']
+})
+export class TaskListItemTeamComponent implements OnInit {
+  @Input() teamIDs: number;
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
