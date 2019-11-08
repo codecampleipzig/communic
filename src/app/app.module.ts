aa13193545bf3e83ec061c3c52cc9f82e696536e
programmiana <m.camargomoreira@gmail.com>
2019-11-08 16:56:44 +0100

    add register card html and css

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { RegisterCardComponent } from './register-card/register-card.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RegisterCardComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/register-card/register-card.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-register-card',
+  templateUrl: './register-card.component.html',
+  styleUrls: ['./register-card.component.css']
+})
+export class RegisterCardComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
