4d21648b48016b4903e6d2ca4434358e42f1129d
MGraf <info@moritz-graf.de>
2019-11-08 16:00:31 +0100

    Join Button Styling

--- src/app/task-list-item-team/task-list-item-team.component.ts
+++ src/app/task-list-item-team/task-list-item-team.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { Task } from '../datatypes/Task';
 
+import { faPlus } from '@fortawesome/free-solid-svg-icons';
+
 @Component({
   selector: 'app-task-list-item-team',
   templateUrl: './task-list-item-team.component.html',
@@ -8,6 +10,8 @@ import { Task } from '../datatypes/Task';
 })
 export class TaskListItemTeamComponent implements OnInit {
   @Input() teamIDs: number;
+
+  faPlus = faPlus;
 
   constructor() { }
 
