f2962c3842a8bdf01cd1cc6203f9a8bd7b463edc
franzwollang <franzwollang@gmail.com>
2019-11-09 16:30:57 +0100

    Holy jesus, Jest finally works...

--- README.md
+++ README.md
@@ -1,3 +1,16 @@
 # Communic
 
 This README will be filled by the project teams, supervised and coordinated by the documentation guru of team Wingardium Codiosaaaa
+
+## Testing
+
+### Commands
+- npm test
+- npm test:watch
+- npm test:ci
+- npm test:coverage
+
+### Notes
+- Use of the TestBed.compileComponents() method is only required when Webpack
+is not being used (or is not configured properly). Check the Webpack configuration
+file if template compilation errors arise.
--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,35 +1,49 @@
-import { TestBed, async } from '@angular/core/testing';
+import { DebugElement } from '@angular/core';
+import { async, TestBed, ComponentFixture } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
 
-xdescribe('AppComponent', () => {
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
+
+describe('AppComponent', () => {
+  beforeEach( () => {
+     TestBed.configureTestingModule({
       imports: [
         RouterTestingModule
       ],
       declarations: [
         AppComponent
       ],
-    }).compileComponents();
-  }));
+    });
+  });
 
   it('should create the app', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
-    expect(app).toBeTruthy();
+    expect.assertions(1);
+    return TestBed.compileComponents()
+      .then( () => {
+        const fixture = TestBed.createComponent(AppComponent);
+        const app = fixture.debugElement.componentInstance;
+        expect(app).toBeTruthy();
+      });
   });
 
   it(`should have as title 'communic'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('communic');
+    expect.assertions(1);
+    return TestBed.compileComponents()
+      .then( () => {
+        const fixture = TestBed.createComponent(AppComponent);
+        const app = fixture.debugElement.componentInstance;
+        expect(app.title).toEqual('communic');
+      });
   });
 
   it('should render title in a h1 tag', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to communic!');
+    expect.assertions(1);
+    return TestBed.compileComponents()
+      .then( () => {
+      const fixture = TestBed.createComponent(AppComponent);
+      fixture.detectChanges();
+      const compiled = fixture.debugElement.nativeElement;
+      expect(compiled.querySelector('h1').textContent).toContain('Welcome to communic!');
+      });
   });
 });
