d2dd247adb14e456461e1edcf2c3041afad699b3
Gabriel Heinrich <gabriel@codecampleipzig.de>
2019-11-12 15:50:13 +0100

    Add project route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,10 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ProjectPageComponent } from './project-page/project-page.component';
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'project/:id', component: ProjectPageComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
