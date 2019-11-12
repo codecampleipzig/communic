a5b4a0559f13375cfde39e9058c4be3d3bb15855
nicolasaranedahinrichs <niko.ah@gmail.com>
2019-11-12 16:16:50 +0100

    projectcardandlist

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,14 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { ProjectListComponent} from './project-list/project-list.component';
+import { ProjectCardComponent} from './project-card/project-card.component';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent, 
+    ProjectListComponent,
+    ProjectCardComponent
   ],
   imports: [
     BrowserModule,
