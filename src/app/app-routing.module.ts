50ea657a2af4ba019bcff1df8719ba88cf772721
Bea Medina <beabloo@protonmail.com>
2019-11-13 09:48:30 +0100

    Change logout to anchor and add routing to login

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,12 +3,15 @@ import { Routes, RouterModule } from '@angular/router';
 import { ProjectPageComponent } from './project-page/project-page.component';
 import { HomeComponent } from './home/home.component';
 import { RegisterCardComponent } from './register-card/register-card.component';
+import { UserActionComponent } from './user-action/user-action.component';
 
 const routes: Routes = [
   {path: 'project/:id', component: ProjectPageComponent},
   {path: 'home', component: HomeComponent},
   {path: '', redirectTo: 'login', pathMatch: 'full'},
-  {path: 'login', component: RegisterCardComponent}
+  {path: 'login', component: RegisterCardComponent},
+  {path: 'login', component: UserActionComponent},
+  {path: 'logout', redirectTo: 'login', pathMatch: 'full'}
 ];
 
 @NgModule({
