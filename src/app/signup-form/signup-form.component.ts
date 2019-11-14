7e68621fa4c4fc8a25f4ab2678a6fda742318503
programmiana <m.camargomoreira@gmail.com>
2019-11-14 10:04:58 +0100

    add password validation

--- src/app/signup-form/signup-form.component.ts
+++ src/app/signup-form/signup-form.component.ts
@@ -1,5 +1,6 @@
 import { Component, NgModule } from '@angular/core';
 import { FormControl, ReactiveFormsModule, Validators, FormGroup } from '@angular/forms';
+import { passwordValidation } from "../passwordvalidator";
 
 
 @NgModule({
@@ -21,8 +22,16 @@ export class SignupFormComponent {
   constructor() { 
     this.profileForm = new FormGroup({
     name: new FormControl ('',[Validators.required]),
-    email: new FormControl ('',[Validators.required, Validators.email]),
-    password: new FormControl('',[Validators.required, Validators.minLength(8)])
+    email: new FormControl ('',Validators.compose([
+      Validators.required,
+      Validators.pattern('^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$')
+
+    ])),
+    password: new FormControl('',Validators.compose([
+      Validators.minLength(8),
+      Validators.required,
+      Validators.pattern('^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])[a-zA-Z0-9]+$') //this is for the letters (both uppercase and lowercase) and numbers validation
+   ])),
   }); 
   }
 
