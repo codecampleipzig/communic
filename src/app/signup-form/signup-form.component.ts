5d49644b6d8b4971f555dc4474e33edd8c1bb145
programmiana <m.camargomoreira@gmail.com>
2019-11-14 15:21:30 +0100

    add password validator

--- src/app/signup-form/signup-form.component.ts
+++ src/app/signup-form/signup-form.component.ts
@@ -1,6 +1,6 @@
 import { Component, NgModule } from '@angular/core';
 import { FormControl, ReactiveFormsModule, Validators, FormGroup } from '@angular/forms';
-import { passwordValidation } from "../passwordvalidator";
+
 
 
 @NgModule({
@@ -22,21 +22,14 @@ export class SignupFormComponent {
   constructor() { 
     this.profileForm = new FormGroup({
     name: new FormControl ('',[Validators.required]),
-    email: new FormControl ('',Validators.compose([
+    email: new FormControl ('',[Validators.required, Validators.email]),
+    password: new FormControl
+    ('',Validators.compose
+    ([
       Validators.required,
-      Validators.pattern('^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$')
-
-    ])),
-    password: new FormControl('',Validators.compose([
-      Validators.minLength(8),
-      Validators.required,
-      Validators.pattern('^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])[a-zA-Z0-9]+$') //this is for the letters (both uppercase and lowercase) and numbers validation
-   ])),
+      Validators.pattern('(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[$@$!%*?&])[A-Za-z\d$@$!%*?&].{8,}') //this is for the letters (both uppercase and lowercase) and numbers validation
+   ]))
   }); 
-  }
-
-  onSubmit() {
-    console.warn(this.profileForm.value);
   }
 
   ngOnInit() {
