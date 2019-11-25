c7ddb4df5d0cd0ad811a154ca83c022caa251e33
Nick <Nick>
2019-11-25 11:29:22 +0100

    Import Task interface

--- src/app/create-new-task/create-new-task.component.ts
+++ src/app/create-new-task/create-new-task.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, HostBinding, NgModule } from "@angular/core";
 import { ReactiveFormsModule, FormGroup, FormBuilder } from "@angular/forms"; 
+import { Task } from "../datatypes/Task";
 
 
 @NgModule({
