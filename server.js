c1b2f254fc3143ce65acc0e6b3c2534edc3b1556
Gabriel Heinrich <gabriel@codecampleipzig.de>
2019-12-22 15:21:47 +0100

    Add https redirect

--- server.js
+++ server.js
@@ -1,7 +1,15 @@
 const express = require("express");
-
 const app = express();
+
 const appFolder = __dirname + "/dist/communic";
+
+app.use((req, res, next) => {
+  if (req.secure) {
+    next();
+  } else {
+    res.redirect("https://" + req.headers.host + req.url);
+  }
+});
 
 app.get("*.*", express.static(appFolder));
 
@@ -11,6 +19,4 @@ app.all("*", (req, res) => {
 
 const port = process.env.PORT || 8080;
 app.listen(port);
-console.log(
-  `Communic Frontend Server is listening on http://localhost:${port}`
-);
+console.log(`Communic Frontend Server is listening on Port ${port}`);
